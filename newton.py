174f191a4dc1ad5d45e6508f18fac4ee9cab8156
cx <balancyxi@mail.com>
2024-08-22 13:42:00 -0700

    add doc and comment

--- newton.py
+++ newton.py
@@ -1,11 +1,39 @@
-def optimize(x0,func,eps=0.1):
-    epsilon=1
-    def derivative(f,x,e=0.01):
-        return (f(x+e)-f(x-e))/(2*e)
-    def second_der(f,x,e=0.01):
-        return (f(x+e)-2*f(x)+f(x-e))/e**2
-    while epsilon>eps:
-        x=x0-derivative(func,x0,e=0.01)/second_der(func,x0,e=0.01)
-        epsilon=x-x0
-        x0=x
+def optimize(x0, func, eps=0.1):
+    """
+    Newton’s method for optimization
+
+    x0: start value
+    func: function
+    eps: epsilon to stop the loop
+    """
+
+    epsilon = 1
+
+    def derivative(f, x, e=0.01):
+        """
+        Estimate the first derivative of f at x using the finite difference method.
+
+        f: objective function
+        x: variable value
+        e: step
+        """
+        return (f(x + e) - f(x - e)) / (2 * e)
+
+    def second_der(f, x, e=0.01):
+        """
+        Estimate the second derivative of f at x using the finite difference method.
+
+        f: objective function
+        x: variable value
+        e: step
+        """
+        return (derivative(f, x + e, e) - derivative(f, x, e)) / e
+
+    while epsilon > eps:  # Stop loop when epsilon is very small
+        x = x0 - derivative(func, x0, e=0.01) / second_der(
+            func, x0, e=0.01
+        )  # Calculate new x
+        epsilon = abs(x - x0)  # Calculate delta (use abs to ensure positive value)
+        x0 = x  # Iterate x
+
     return x0
